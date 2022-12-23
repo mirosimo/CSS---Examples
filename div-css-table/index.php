0c90907654512322cfea39390d858ca4aadccd8f
mirosimo <120422945+mirosimo@users.noreply.github.com>
2022-12-23 22:51:56 +0100

    Create index.php

---
+++ div-css-table/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>DIV CSS Table</title>
+        <link rel="stylesheet" href="style.css?<?php echo time(); ?>" />            
+    </head>
+    <body>        
+            <div class="title_part">		
+		<h1>Seznam zákazníků</h1>
+            </div>
+            
+            <div class="table_part">			  			    	
+	    	<div class="m_table">
+                    <div class="table_head">
+	      		<span>Jméno</span> <span>Příjmení</span> <span>Město</span>
+                    </div>
+                    <div class="table_body">
+                        <div> <span>Karel</span> <span>Vosáhlo</span> <span>Praha</span> </div>	      		
+                        <div> <span>Aleš</span> <span>Petrásek</span> <span>Beroun</span> </div>                                                     	      		
+                        <div> <span>Ladislav</span> <span>Procházka</span> <span>Lanškroun</span> </div>	      		
+                        <div> <span>Václav</span> <span>Veselovský</span> <span>Příbram</span> </div>
+                        <div> <span>Zdeněk</span> <span>Novák</span> <span>Praha</span> </div>
+                        <div> <span>Petr</span> <span>Kmeťovský</span> <span>Slaný</span> </div>
+                        <div> <span>Pavel</span> <span>Žák</span> <span>Kolín</span> </div>
+                    </div>
+	      	</div>
+            </div>                  		
+    </body>
+</html>

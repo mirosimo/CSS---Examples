b5daaa8d38ac4c83bbbf54bb2c6ebad5bb2522d9
mirosimo <120422945+mirosimo@users.noreply.github.com>
2022-12-29 08:58:16 +0100

    Update index.php

--- div-css-table/index.php
+++ div-css-table/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <title>DIV CSS Table</title>
-        <link rel="stylesheet" href="style.css?<?php echo time(); ?>" />            
+        <link rel="stylesheet" href="css/style2.css?<?php echo time(); ?>" />            
     </head>
     <body>        
             <div class="title_part">		
@@ -10,20 +10,25 @@
             </div>
             
             <div class="table_part">			  			    	
-	    	<div class="m_table">
-                    <div class="table_head">
-	      		<span>Jméno</span> <span>Příjmení</span> <span>Město</span>
-                    </div>
-                    <div class="table_body">
-                        <div> <span>Karel</span> <span>Vosáhlo</span> <span>Praha</span> </div>	      		
-                        <div> <span>Aleš</span> <span>Petrásek</span> <span>Beroun</span> </div>                                                     	      		
-                        <div> <span>Ladislav</span> <span>Procházka</span> <span>Lanškroun</span> </div>	      		
-                        <div> <span>Václav</span> <span>Veselovský</span> <span>Příbram</span> </div>
-                        <div> <span>Zdeněk</span> <span>Novák</span> <span>Praha</span> </div>
-                        <div> <span>Petr</span> <span>Kmeťovský</span> <span>Slaný</span> </div>
-                        <div> <span>Pavel</span> <span>Žák</span> <span>Kolín</span> </div>
-                    </div>
-	      	</div>
-            </div>                  		
+	    	<table cellspacing="0" cellpadding="0">
+                    <tr class="row_head">
+	      		<th>Jméno</th> 
+                        <th>Příjmení</th> 
+                        <th>Město</th>
+                    </tr>
+                        <tr class="row_body"> <td>Karel</td> <td>Vosáhlo</td> <td>Praha</td> </tr>	      		
+                        <tr class="row_body"> <td>Aleš</td> <td>Petrásek</td> <td>Beroun</td> </tr>                                                     	      		
+                        <tr class="row_body"> <td>Ladislav</td> <td>Procházka</td> <td>Lanškroun</td> </tr>	      		
+                        <tr class="row_body"> <td>Václav</td> <td>Veselovský</td> <td>Příbram</td> </tr>
+                        <tr class="row_body"> <td>Zdeněk</td> <td>Novák</td> <td>Praha</td> </tr>
+                        <tr class="row_body"> <td>Petr</td> <td>Kmeťovský</td> <td>Slaný</td> </tr>
+                        <tr class="row_body"> <td>Pavel</td> <td>Žák</td> <td>Kolín</td> </tr>                    
+
+	      	</table>
+            </div>
+        
+            <div class="buttons_part">		
+		<a href="#">Přidej záznam</a>
+            </div>
     </body>
 </html>

f928291a6f20fdbf681466a3b4297265111e452a
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-03-20 12:24:55 +0100

    Add files via upload

---
+++ flex/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>FLEX - Top Part of Page</title>
+        <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" />            
+    </head>
+    <body>
+          <div class="body-cover">  
+          <div class="top-part">              
+              <img class="main-logo-img" src="img/logo.png" />	
+              <img class="top-img" src="img/sky-line2.jpg" />
+              <img class="top-img" src="img/porsche_2.jpg" />              
+          </div>
+          </div>
+    </body>
+</html>

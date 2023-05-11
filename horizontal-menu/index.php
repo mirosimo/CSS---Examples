10ec4bf19aee96f7621807e6987a8790ca826f5d
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-05-11 09:20:31 +0200

    Add files via upload

---
+++ horizontal-menu/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+            <meta charset="utf-8"> 
+            <title>Horizontal Menu</title>
+            <link rel="stylesheet" href="style.css?<?php echo time(); ?>" />            
+	</head>
+	<body>
+            <nav class="m-navbar">
+                <ul id="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Computers</a></li>
+                    <li><a href="#">Mobile Phones</a>
+                        <ul>
+                            <li>
+                                <a href="#">Smart Phones</a>
+                                <ul>
+                                    <li><a href="#">Samsung</a></li>
+                                    <li><a href="#">Apple</a></li>
+                                    <li><a href="#">Honor</a></li>
+                                    <li><a href="#">Xiaomi</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Feature Phones</a>
+                                <ul>
+                                    <li><a href="#">Samsung</a></li>
+                                    <li><a href="#">Apple</a></li>
+                                    <li><a href="#">Honor</a></li>
+                                    <li><a href="#">Xiaomi</a></li>`
+                                </ul>
+                            </li>
+                            <li><a href="#">Rugged Phones</a>
+                                <ul>
+                                    <li><a href="#">Samsung</a></li>
+                                    <li><a href="#">Apple</a></li>
+                                    <li><a href="#">Honor</a></li>
+                                    <li><a href="#">Xiaomi</a></li>
+                                </ul>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Audio</a></li>                    
+                </ul>           
+            </nav>
+	</body>
+</html>

fe3293d1210998311db4ef27e1d8fa7988b1b349
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-06-30 19:54:19 +0200

    Add files via upload

---
+++ spread-inner-shape-to-outer-border/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Spread Circle to Rectangle</title>
+        <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" />            
+    </head>
+    <body>
+         <div class="card">    
+             <div class="circle">                 
+             </div>
+         </div>
+    </body>
+</html>

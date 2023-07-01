4a51f39e3243ed0760ac1227ce152236fe12e6d7
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-07-01 18:26:08 +0200

    Add files via upload

---
+++ item-detail/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Item detail</title>
+        <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" />            
+    </head>
+    <body>
+         <div class="m-card">    
+             <div class="circle">                 
+             </div>
+             <div class="content">
+                 <h2>Herní křeslo s polštáři</h2>
+                 <p>
+                     Otočné herní křeslo pro milovníky počítačových her. 
+                     Je nepostradatelnou věcí pro nadšené hráče. Struktura křesla zaručuje plný komfort používání 
+                     během mnoha hodin her nebo práce.
+                 </p>
+                 <a href="#">Koupit Teď</a>
+             </div>
+             <img src="img/pc-zidle.png">
+         </div>
+    </body>
+</html>

4a182afa42371ab3be43ae208d8ca404f002d619
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-07-19 23:20:44 +0200

    Add files via upload

---
+++ rotate-cube/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Cube Rotation</title>
+        <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" />            
+    </head>
+    <body>
+        <div id = "cube-container">
+            <div class = "cube">		
+                    <img src = "img/meal_1.jpg">
+                    <img src = "img/meal_4.jpg">
+                    <img src = "img/meal_2.jpg">
+                    <img src = "img/meal_3.jpg">
+            </div>     
+        </div>    
+    </body>
+</html>

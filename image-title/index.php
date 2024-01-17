c472f6ec0755d0b10df4e977e8841218fc3328ce
mirosimo <120422945+mirosimo@users.noreply.github.com>
2024-01-17 21:22:34 +0100

    Add files via upload

---
+++ image-title/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">  
+  <link rel="stylesheet" href="style.css">
+  <title>FEM grid</title>
+</head>
+
+<body>
+  <div class="img-grid-container">
+      <div class="img-wrapper">
+          <img src="opice.jpg" alt="Keep Smiling !!!">
+      </div>
+      <div class="title-wrapper">
+          <span>Hallo my friends</span>
+      </div>
+  </div>
+</body>
+
+</html>

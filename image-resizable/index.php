aae08878a65809075c522eec284de9f1869db9ff
mirosimo <120422945+mirosimo@users.noreply.github.com>
2024-02-11 10:42:19 +0100

    Add files via upload

---
+++ image-resizable/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">  
+  <link rel="stylesheet" href="style.css?time=<?php echo time();?>">
+  <title>Image Resizible</title>
+</head>
+
+<!-- Example where image size is adjusted to the size of outer div container -->
+
+<body>  
+      <div class="img-wrapper">                    
+          <img class="main-img" src="monkey.jpg" alt="Monkey image">          
+      </div>          
+</body>
+
+</html>
